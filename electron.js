c28d86dbfb9059062625783f6b70d8988a3e9eba
Nick Oliver <github@pixnbits.org>
2014-06-26 23:24:39 -0700

    Issue #34: Detect board model on serial port uses a workaround for issue on Windows of not populating vendor and product IDs

--- electron.js
+++ electron.js
@@ -43,6 +43,18 @@ app.use(express.static(__dirname+'/public'));
 
 app.get('/ports',function(req,res) {
     sp.list(function(err,list) {
+
+        // format the data (workaround serialport issue on Win (&*nix?))
+        list.forEach(function(port) {
+            if(port.pnpId){
+                var data = /^USB\\VID_([a-fA-F0-9]{4})\&PID_([a-fA-F0-9]{4})/.exec(port.pnpId);
+                if(data){
+                    port.vendorId = port.vendorId || '0x'+data[1];
+                    port.productId = port.productId || '0x'+data[2];
+                }
+            }
+        });
+
         res.send(JSON.stringify(list));
         res.end();
     });
