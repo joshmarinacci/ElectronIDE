669e5554154441afd6a706b4d9c52ba5a7558ebc
Josh Marinacci <joshua@marinacci.org>
2014-07-03 18:55:11 -0700

    recursively download platforms. move download code to util

--- platform.js
+++ platform.js
@@ -1,9 +1,7 @@
 var fs = require('fs');
-var AdmZip = require('adm-zip');
 var settings = require('./settings');
-var zlib = require('zlib');
-var tar = require('tar');
 var http = require('http');
+var util = require('./util');
 
 //var CLOUDPATH = "/Users/josh/projects/ArduinoZips";
 var CLOUDPATH = 'http://joshondesign.com/p/apps/electron/platforms';
@@ -80,31 +78,8 @@ function Platform() {
             cb();
             return;
         }
-        console.log("not installed. installing now");
         var zippath = CLOUDPATH+'/'+VERSION+'/arduino-'+VERSION+'-'+this.os+'-trimmed.tar.gz';
-        console.log('zip path = ',zippath);
-
-        var outpath = this.root;
-        console.log('unziping to ',outpath);
-        var req = http.get(zippath);
-        req.on('response', function(res) {
-            var total = res.headers['content-length']; //total byte length
-            var count = 0;
-            res
-                .on('data', function(data) {
-                    count += data.length;
-                    if(update) update( {message:count/total});
-                })
-                .pipe(zlib.createGunzip())
-                .pipe(tar.Extract({path:outpath, strip: 1}))
-                .on('error',function() {
-                    console.log('there was an error');
-                })
-                .on('close',function() {
-                    console.log('unzipped the files');
-                    if(cb) cb();
-                });
-            });
+        util.downloadUntgzTo(zippath,this.root,update, cb);
     }
 }
 var _default = new Platform();
@@ -114,69 +89,40 @@ _digispark_pro.getStandardLibraryPath = function() {   return this.droot + '/lib
 _digispark_pro.getCorePath = function(device) { return this.droot + '/cores/'+device.build.core;   }
 _digispark_pro.getVariantPath = function(device) { return this.droot + '/variants/'+device.build.core;   }
 _digispark_pro.getAvrDudeBinary = function(device) { return this.droot + '/tools/avrdude'; }
+_digispark_pro.parentPlatform = _default;
 
 var _trinket3 = new Platform();
 _trinket3.id = 'trinket3';
-//_trinket3.hroot = '/Users/josh/Downloads/hardware/attiny';
 _trinket3.hroot = _trinket3.getReposPath() + '/hardware/'+_trinket3.id;
-console.log("trinket hroot = ",_trinket3.hroot);
 
 _trinket3.parentPlatform = _default;
 _trinket3.getVariantPath = function(device) {   return this.hroot + '/hardware/attiny/variants/' + device.build.variant;  }
 _trinket3.isInstalled = function() { return fs.existsSync(this.hroot);  }
 
-function downloadUnzipTo(remote, outpath, update, cb) {
-    console.log('zip path = ',remote);
-    var self = this;
-    console.log('unziping to ',outpath);
-    var req = http.get(remote);
-    var fout = fs.createWriteStream('/tmp/blah.zip');
-    req.on('response', function(res) {
-        var total = res.headers['content-length']; //total byte length
-        var count = 0;
-        res
-            .on('data', function(data) {
-                count += data.length;
-                if(update) update( {message:count/total});
-            })
-            .pipe(fout)
-            .on('error',function() {
-                console.log('there was an error');
-            })
-            .on('close',function() {
-                console.log('unzipped the files');
-                var zip = new AdmZip('/tmp/blah.zip');
-                zip.extractAllTo(outpath,true);
-                if(cb) cb();
-            });
-        });
-}
-
 _trinket3.installIfNeeded = function(cb,update) {
-        if(this.isInstalled()) {
+    var self = this;
+    this.parentPlatform.installIfNeeded(function() {
+        if(self.isInstalled()) {
             cb();
             return;
         }
-        console.log("not installed. installing now");
-        //http://learn.adafruit.com/system/assets/assets/000/010/777/original/trinkethardwaresupport.zip?1378321062
         var zippath = 'http://learn.adafruit.com/system/assets/assets/000/010/777/original/trinkethardwaresupport.zip?1378321062';
-        var path = this.hroot;
-        downloadUnzipTo(zippath,path,update, function() {
+        var path = self.hroot;
+        util.downloadUnzipTo(zippath,path,update, function() {
             var confpath = 'http://learn.adafruit.com/system/assets/assets/000/010/980/original/avrdudeconfmac.zip?1379342581';
-            downloadUnzipTo(confpath,path, update, cb);
+            util.downloadUnzipTo(confpath,path, update, cb);
         });
+    },update);
 }
 _trinket3.useSerial = function() { return false; }
 _trinket3.getAvrDudeConf = function(device) { return this.hroot + '/avrdude.conf'; }
 _trinket3.getProgrammerId = function() { return 'usbtiny'; }
 
 exports.getDefaultPlatform = function() {
-    console.log('getting the default platform');
     return _default;
 }
 
 exports.getPlatform = function(device) {
-    console.log(device);
     if(device.id == 'digispark-pro') return _digispark_pro;
     if(device.id == 'digispark') return _digispark_pro;
     if(device.id == 'trinket3') return _trinket3;
---
+++ util.js
@@ -0,0 +1,60 @@
+var http = require('http');
+var fs = require('fs');
+var AdmZip = require('adm-zip');
+var zlib = require('zlib');
+var tar = require('tar');
+
+exports.downloadUnzipTo = function (remote, outpath, update, cb) {
+    console.log('downloading ',remote);
+    var self = this;
+    console.log('unziping to ',outpath);
+    var req = http.get(remote);
+    var fout = fs.createWriteStream('/tmp/blah.zip');
+    req.on('response', function(res) {
+        var total = res.headers['content-length']; //total byte length
+        var count = 0;
+        res
+            .on('data', function(data) {
+                count += data.length;
+                if(update) update( {message:count/total});
+            })
+            .pipe(fout)
+            .on('error',function(err) {
+                console.log('there was an error');
+                if(cb) cb(new Error(""+err));
+            })
+            .on('close',function() {
+                console.log('unzipped the files');
+                var zip = new AdmZip('/tmp/blah.zip');
+                zip.extractAllTo(outpath,true);
+                if(cb) cb();
+            });
+        });
+}
+
+exports.downloadUntgzTo = function (remote, outpath, update, cb) {
+    console.log('downloading ',remote);
+    var self = this;
+    console.log('unziping to ',outpath);
+    var req = http.get(remote);
+    var fout = fs.createWriteStream('/tmp/blah.zip');
+    req.on('response', function(res) {
+        var total = res.headers['content-length']; //total byte length
+        var count = 0;
+        res
+            .on('data', function(data) {
+                count += data.length;
+                if(update) update( {message:count/total});
+            })
+            .pipe(zlib.createGunzip())
+            .pipe(tar.Extract({path:outpath, strip: 1}))
+            .on('error',function(err) {
+                console.log('there was an error');
+                if(cb) cb(new Error(""+err));
+            })
+            .on('close',function() {
+                console.log('unzipped the files');
+                if(cb) cb();
+            });
+        });
+}
