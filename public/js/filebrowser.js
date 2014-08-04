16e999f3bc7bdeb736a2ee7eaaf8f743aa17fd18
Josh Marinacci <joshua@marinacci.org>
2014-08-04 10:08:37 -0700

    make file tree nodes collapse

--- public/js/filebrowser.js
+++ public/js/filebrowser.js
@@ -7,4 +7,17 @@ app.controller('FilebrowserCtrl', ['Sketch','$http',function(Sketch,$http){
         this.loadFile = function(file) {
             Sketch.loadSketch(file);
         }
+
+        this.toggleNode = function(node) {
+            node.closed = !node.closed;
+        }
+        this.isClosed = function(node) {
+            if(node.closed == undefined) {
+                node.closed = false;
+            }
+            return {
+                'tree-node-closed':node.closed,
+                'tree-node-opened':!node.closed
+            }
+        }
     }]);
